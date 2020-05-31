f0ef2eae32d40c88982a8c7f76a2453831c87860
daksh31pandey <daksh31pandey@gmail.com>
2020-05-31 13:54:40 +0530

    loading and cleaning data

---
+++ loading_cleaning_data.R
@@ -0,0 +1,11 @@
+#loading and cleaning data
+
+t <- read.table("household_power_consumption.txt", header=TRUE, sep=";", na.strings = "?", colClasses = c('character','character','numeric','numeric','numeric','numeric','numeric','numeric','numeric'))
+t$Date<-as.Date(t$Date,"%d/%m/%Y")
+t<-subset(t,Date>=as.Date("2007-02-01") & Date<=as.Date("2007-02-02"))
+t <- t[complete.cases(t),]
+dateTime <- paste(t$Date, t$Time)
+dateTime <- setNames(dateTime, "DateTime")
+t <- t[ ,!(names(t) %in% c("Date","Time"))]
+t <- cbind(dateTime, t)
+t$dateTime <- as.POSIXct(dateTime)
